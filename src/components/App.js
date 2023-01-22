474db147c85ed721c4783fe455303827535883eb
Iris <iris.thuo@student.moringaschool.com>
2023-01-22 05:49:35 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
@@ -6,10 +6,27 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
+  const [question , setQuestion] = useState([])
+
+  useEffect(() => {
+        fetch("http://localhost:4000/questions")
+        .then((res) => res.json())
+        .then((data) => setQuestion(data))
+  },[])
+
+  function addQuestions(newQuestion) {
+    setQuestion([...question, newQuestion])
+  }
+
+  function deleteQuestion(id) {
+    const updatedQuestions = question.filter(oldQuestion => oldQuestion.id !== id)
+    setQuestion(updatedQuestions)
+  }
+
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm addQuestions={addQuestions}/> : <QuestionList questions={question} deleteQuestion={deleteQuestion}/>}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question , deleteQuestion}) {
+    
+
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +10,14 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+
+  function handleClick() {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE"
+    })
+      .then(response => response.json())
+      .then(() => deleteQuestion(question.id))
+  }
 
   return (
     <li>
@@ -17,7 +27,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleClick}>Delete Question</button>
     </li>
   );
 }
